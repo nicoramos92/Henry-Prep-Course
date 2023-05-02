24eea5dbd22014b49f3aa3655289b1f5215d09aa
NicoRamos <nicoramos92@gmail.com>
2023-05-02 17:18:02 -0300

    M06 - incompleto

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,6 +7,13 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+   var gato = {
+      nombre: nombre,
+      edad: edad,
+      meow: function () {return "Meow!"}
+   }
+
+   return gato;
 }
 
 function nuevoUsuario(nombre, email, password) {
@@ -14,6 +21,13 @@ function nuevoUsuario(nombre, email, password) {
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
    // Tu código:
+   var user = {
+      nombre: nombre,
+      email: email,
+      password: password,
+   }
+
+   return user;
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +36,9 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+
+   objeto[propiedad] = null;
+   return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,6 +46,7 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+   objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
